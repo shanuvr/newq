687f65d2ffb32793a2db343c8ddcfd1a7a087ce1
shanu <shanuvr112233@gmail.com>
2025-09-18 17:14:38 +0530

    first commit

---
+++ exp.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+app.listen(3000)
+const arr= [];
+ app.post('/add',(req,res)=>{
+    const {email} = req.body;
+    arr.push(email)
+ })
+
+ app.get('/get',(req,res)=>{
+    res.send(arr)
+ })
+ app.delete('/delete',(req,res)=>{
+   const id = req.params
+   arr.splice(id,1)
+   
+ })
